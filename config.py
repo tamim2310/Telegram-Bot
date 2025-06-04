67a21ccc716cdf66064876f1e1c4b78cfff4c4fe
tamim2310 <tamimbro2310@gmail.com>
2025-06-04 19:30:19 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# Bot configuration
+BOT_TOKEN = "7709244900:AAEtjoqKi69pAaDNWCaIMVOCHoGJtQkj-1w"
+ADMIN_ID = 7318787235
+CHANNEL_ID = "-1002516369711"
+
+# External links
+YOUTUBE_LINK = "https://youtube.com/@potiexai?si=pVihkwgvZobjW9oA"
+TELEGRAM_LINK = "https://t.me/+FczZ2OHZue9iYWI9"
+POCKET_OPTION_LINK = "https://pocket-friends.com/r/xnkeklqyyl"
+QUOTEX_LINK = "https://broker-qx.pro/?lid=1377793"
+SUBSCRIBE_LINK = "https://t.me/+FczZ2OHZue9iYWI9"
